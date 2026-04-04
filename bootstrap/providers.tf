657bb41c4a0774582dbc02eafa68e031fefc4343
Pietro Rampazzo <pietro@rampazzo.eu>
2026-04-04 13:59:35 +0200

    fix formatting

--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -12,10 +12,10 @@ terraform {
   # After the first manual `terraform apply`, uncomment this backend block,
   # then run:  terraform init -migrate-state
   # -----------------------------------------------------------------------
-   backend "s3" {
-     bucket = "rampazzo-tfstate"
-     key    = "bootstrap.tfstate"
-     region = "eu-north-1"
+  backend "s3" {
+    bucket = "rampazzo-tfstate"
+    key    = "bootstrap.tfstate"
+    region = "eu-north-1"
   }
 }
 
--- bootstrap/terraform.tfvars
+++ bootstrap/terraform.tfvars
@@ -1,5 +1,5 @@
-aws_region           = "eu-north-1"
-state_bucket_name    = "rampazzo-tfstate"
+aws_region        = "eu-north-1"
+state_bucket_name = "rampazzo-tfstate"
 allowed_github_repos = [
-    "peterampazzo/aws-infra"
+  "peterampazzo/aws-infra"
 ]
